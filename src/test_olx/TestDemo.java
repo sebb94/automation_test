06ed1151f3d0e56c80da0410723ea21110b919bb
=Seba <=omgl0lwth@gmail.com>
2020-04-12 20:39:38 +0200

    initial commit

---
+++ src/test_olx/TestDemo.java
@@ -0,0 +1,24 @@
+package test_olx;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TestDemo {
+
+  @BeforeClass
+  public void beforeClass() {
+	  
+  }
+  
+  @Test
+  public void test1() {
+	  System.out.println("TEST");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  
+  }
+
+}
