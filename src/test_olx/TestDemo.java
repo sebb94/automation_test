a0e35c284a22205ec149193402ab02e65da15df7
=Seba <=omgl0lwth@gmail.com>
2020-04-13 10:30:10 +0200

    initial setup

--- src/test_olx/TestDemo.java
+++ src/test_olx/TestDemo.java
@@ -2,13 +2,26 @@ package test_olx;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 
 public class TestDemo {
+	
+   WebDriver driver;
+   String baseURL;
 
   @BeforeClass
   public void beforeClass() {
-	  
+	    baseURL = "https://olx.pl";	
+	    driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(baseURL);
   }
   
   @Test
@@ -18,6 +31,7 @@ public class TestDemo {
 
   @AfterClass
   public void afterClass() {
+	  driver.quit();
 	  
   }
 
