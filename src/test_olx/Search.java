7739e4284edb3540b99ee1c5861a7ef6b02922d8
=Seba <=omgl0lwth@gmail.com>
2020-04-13 17:44:49 +0200

    reports with screenshots on failure, xml to run test suite, imporve search methods

--- src/test_olx/Search.java
+++ src/test_olx/Search.java
@@ -1,26 +1,47 @@
 package test_olx;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
+
 
 public class Search {
 	
 	WebDriver driver = null;
 	ExtentTest test;
 	WebDriverWait wait;
+	private String carModel;
+	private String carBrand;
+	private String city;
 	
 	public Search(WebDriver driver, ExtentTest test) {
 		this.driver = driver;
 		this.test = test;
-		this.wait = new WebDriverWait(driver, 5);
+		this.wait = new WebDriverWait(driver,5);
 	}
 	
+	public void acceptCookies() {
+		driver.findElement(By.xpath("//div[@id=\"cookiesBar\"]//button")).click();
+	}
+	
+	public void takeScreenShot() throws Exception {
+		String path = Screenshots.takeScreenshot(driver);
+		String imagePath = test.addScreenCapture(path);
+		test.log(LogStatus.FAIL, "", imagePath);
+	
+	}
+	
+	public void info() {
+		test.log(LogStatus.INFO, "City: " + city);
+		test.log(LogStatus.INFO, "Car Brand: " + carBrand);
+		test.log(LogStatus.INFO, "Car Model: " + carModel);
+	}
 	public void goToCarsSearch() throws Exception {
 		  driver.findElement(By.xpath("//div[@class='maincategories']//span[text()=\"Motoryzacja\"]")).click();
 		  Thread.sleep(500);
@@ -28,32 +49,58 @@ public class Search {
 		  Thread.sleep(500);
 	}
 	
+	public void cityFill(String city) throws Exception {
+		this.city = city;
+		try {	
+		WebElement cityField = wait.until(ExpectedConditions.elementToBeClickable(By.id("cityField")));
+		cityField.sendKeys(city);
+		WebElement cityFieldValue = wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//a[contains(@class,'search-choose-selected')]//strong[text()='"+ city +"']")));
+		 Thread.sleep(1000);
+		cityFieldValue.click();
+	  	 test.log(LogStatus.PASS, "City find correct");
+		} catch(Exception e) {
+			takeScreenShot();
+			test.log(LogStatus.FAIL, "Miasto: " + e.getMessage());
+		}
+	}
 	
-	public void carBrand(String carBrand) {
-		  carBrand = carBrand.toLowerCase();
-		  WebElement carBrandElement = driver.findElement(By.xpath("//span[text()='Marka']"));
-		  carBrandElement.click();
+	
+	public void carBrand(String carBrand) throws Exception {
+		  this.carBrand = carBrand;
 		  try {
-			  WebElement carBrandValue =  wait.until
+		  carBrand = carBrand.toLowerCase();
+		  WebElement carBrandElement = wait.until
+				  	(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Marka']")));
+		  Thread.sleep(1000);
+		  carBrandElement.click();	  
+		  WebElement carBrandValue =  wait.until
 				  	(ExpectedConditions.elementToBeClickable(By.xpath("//a[@data-code='"+ carBrand +"']")));
+		   Thread.sleep(1000);
 			  carBrandValue.click();
-			  test.log(LogStatus.INFO, "Car Brand find correct");
+			  test.log(LogStatus.PASS, "Car Brand find correct");
 		  }catch(Exception e) {
-			  test.log(LogStatus.FAIL, e.getMessage());
+			  takeScreenShot();
+			  test.log(LogStatus.FAIL, "Marka: " + e.getMessage());
 		  }	
 	}
 	
-	public void carModel(String carModel) {
-		  WebElement carModelElement = driver.findElement(By.xpath("//span[text()='Model']"));
-		  carModelElement.click();
+	public void carModel(String carModel) throws Exception {
+		  this.carModel = carModel;
 		  try {
+		  WebElement carModelElement = wait.until
+				  	(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Model']")));
+		  Thread.sleep(1000);
+		  carModelElement.click();
 			  WebElement carModelValue =  wait.until
 				  	(ExpectedConditions.elementToBeClickable
-				  	(By.xpath("//li[@data-key='model']//span[text()='"+ carModel + "']")));
+				  	(By.xpath("//li//label[@data-text='"+ carModel +"']")));
+			  Thread.sleep(1000);
 			  carModelValue.click();
-			  test.log(LogStatus.INFO, "Car Model find correct");
+			  test.log(LogStatus.PASS, "Car Model find correct");
 		  }catch(Exception e) {
-			  test.log(LogStatus.FAIL, e.getMessage());
+			  takeScreenShot();
+			  test.log(LogStatus.FAIL, "Model: " + e.getMessage());
 		  }	
 		
 	}
--- src/test_olx/TestDemo.java
+++ src/test_olx/TestDemo.java
@@ -4,14 +4,20 @@ import org.testng.annotations.Test;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 
 public class TestDemo {
@@ -25,10 +31,12 @@ public class TestDemo {
 
   @BeforeClass
   public void beforeClass() {
+	  	report = ExtentFactory.getInstance();
+	  	LocalDateTime now = LocalDateTime.now();  
+	  	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy -  HH:mm:ss"); 
 	    baseURL = "https://olx.pl";	
-	    driver = new ChromeDriver(); 
-	    report = ExtentFactory.getInstance();
-	    test = report.startTest("Verify cars search");
+	    driver = new ChromeDriver();	 
+	    test = report.startTest("Verify cars search " + dtf.format(now));
 	    search = new Search(driver, test);
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
@@ -36,11 +44,13 @@ public class TestDemo {
   }
   
   @Test
-  public void test1() throws Exception {
+  public void test1() throws Exception { 
+	  search.acceptCookies();
 	  search.goToCarsSearch();
+	  search.cityFill("Kraków");
 	  search.carBrand("BMW");
 	  search.carModel("Seria 33");
-	  
+	  search.info();
 	 
   }
 
